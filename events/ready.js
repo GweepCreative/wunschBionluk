7a83e364b921e03e9de57945aa85e91ac55ea232
Grkn <69747065+GweepCreative@users.noreply.github.com>
2024-06-11 13:14:09 +0300

    Delete events directory

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client) => {
-  console.log(`${client.user.tag} ismi ile giriş yapıldı`);
-  client.user.setPresence({
-    activities: [{ name: "Sanalika" }],
-    status: "idle",
-  });
-};
