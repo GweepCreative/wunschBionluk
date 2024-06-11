240dfcdd300f65459a85455a6f2726720b905be8
Grkn <69747065+GweepCreative@users.noreply.github.com>
2024-06-11 13:13:46 +0300

    Delete utils directory

--- utils/formatTime.js
+++
@@ -1,9 +0,0 @@
-function formatTime(string) {
-  return string
-    .replace("s", " saniye")
-    .replace("m", " dakika")
-    .replace("h", " saat")
-    .replace("d", " gün")
-    .replace("w", " hafta");
-}
-module.exports = formatTime;
--- utils/shop.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Shop = new mongoose.Schema({
-    id: { type: String, unique: true, required: true },
-    name: { type: String, default: "" },
-    balance: { type: Number, default: 0 },
-});
-
-module.exports = mongoose.model("Shop", Shop);
